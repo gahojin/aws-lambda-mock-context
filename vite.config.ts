4b5af87e14ed46e8781437ea75016819e7004fe1
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-05-02 21:08:37 +0900

    build: バンドラをrolldownからviteに変更する
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [
+    dts({
+      exclude: ['src/**/*.test.ts'],
+    }),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  build: {
+    ssr: true,
+    lib: {
+      entry: ['src/index.ts'],
+      formats: ['es'],
+    },
+    minify: false,
+    sourcemap: true,
+    rolldownOptions: {
+      treeshake: true,
+      output: {
+        cleanDir: true,
+        comments: false,
+      },
+      external: [],
+      optimization: {
+        inlineConst: { mode: 'all', pass: 5 },
+      },
+      experimental: {
+        nativeMagicString: true,
+      },
+    },
+  },
+})
